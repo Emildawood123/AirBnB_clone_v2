427d7ddf403b3cda317a507cfe2a56049e7c2811
Emil dawood <edawood997@gmail.com>
2023-12-24 15:14:17 +0200

    i don't have any thing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,7 +2,7 @@
 """import flask moudle"""
 
 from flask import Flask, render_template
-from models import storage
+from models import FileStorage
 from models.state import State
 
 
@@ -12,7 +12,7 @@ app = Flask()
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """states_list fun"""
-    dic = storage.all(State).values()
+    dic = FileStorage().all(State).values()
     sorted = sorted(dic, key=lambda y: y.name)
     return render_template('7-states_list.html', sorted_list=sorted)
 
@@ -20,7 +20,7 @@ def states_list():
 @app.teardown_appcontext
 def remove_and_reset():
     """remove and reset"""
-    storage.close()
+    FileStorage().close()
 
 
 if __name__ == "__main__":
