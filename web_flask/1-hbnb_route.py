c3719b852a0b41b00e5417fd17d0e70e7f7f9e61
Emil dawood <edawood997@gmail.com>
2023-12-24 13:29:56 +0200

    second task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,21 @@
+#!/usr/bin/python3
+"""Flask import moudle"""
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', strict_slashes=False)
 def home():
+    """function return what in HomePage"""
     return "Hello HBNB!"
 
-@app.route('/hbnb')
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """content of route /hbnb"""
     return "HBNB"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
