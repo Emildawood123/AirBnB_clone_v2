e8235653a4375bdac34fa482caa4cf155fb693d0
Emil dawood <edawood997@gmail.com>
2023-12-24 14:22:51 +0200

    eighth task

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""import flask moudle"""
+
+from flask import Flask, render_template
+from models.state import State
+from models import storage
+app = Flask()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    dic = storage.all(State).values()
+    sorted = sorted(dic, key=lambda y: y.name)
+    return render_template('7-states_list.html', sorted_list=sorted)
+
+
+@app.teardown_appcontext
+def remove_and_reset():
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
