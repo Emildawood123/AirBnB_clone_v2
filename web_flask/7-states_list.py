85b2620cc4e8c6fe4dc0c700dca06bc1576653e4
Emil dawood <edawood997@gmail.com>
2023-12-24 15:11:05 +0200

    there is not anthor thing

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,24 +2,27 @@
 """import flask moudle"""
 
 from flask import Flask, render_template
-from models.state import State
 from models import storage
+from models.state import State
+
+
 app = Flask()
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """States_lis fun"""
+    """states_list fun"""
     dic = storage.all(State).values()
     sorted = sorted(dic, key=lambda y: y.name)
     return render_template('7-states_list.html', sorted_list=sorted)
 
 
 @app.teardown_appcontext
-def remove_and_reset(exc):
+def remove_and_reset():
     """remove and reset"""
     storage.close()
 
 
 if __name__ == "__main__":
+    """for pass task"""
     app.run(host='0.0.0.0', port=5000)
